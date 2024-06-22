d64f071394f0aa17273d89c54e4272bd925e5f7f
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-06-22 19:51:36 +0700

    Add files via upload

--- scene1.js
+++ scene1.js
@@ -10,7 +10,7 @@ class scene1 extends Phaser.Scene {
   create() {
     //Create A Player And Make It Drag
     this.player = this.add.rectangle(config.width /2, config.height /2 + 400, 120, 30, 0xf00000);
-    this.physics.add.existing(this.player, true)
+    this.physics.add.existing(this.player)
     
     this.player.setInteractive();
     this.input.setDraggable(this.player);
@@ -18,16 +18,26 @@ class scene1 extends Phaser.Scene {
       object.x = dragX;
       
     });
+    this.player.body.setCollideWorldBounds(true)
+    this.player.body.setImmovable(true)
     
      //Create A circle Then Add The Physics
     this.circle = this.add.circle(config.width /2 , config.height / 2, 50, 0xf00000);
      this.physics.add.existing(this.circle)
-    this.circle.body.velocity.y = 200
-    this.circle.body.setBounce(2, 2)
-    this.physics.add.collider(this.player, this.circle)
+    
+    this.circle.body.velocity.y = 500
+    this.circle.body.setBounce(1, 1)
+    this.circle.body.setCollideWorldBounds(true)
+    
+     //Make It Callback If The Objects Got Touched
+    this.physics.add.collider(this.player, this.circle, (object1, object2) => {
+      object2.body.velocity.x += 100
+      
+    })
   }
   
   update() {
+    
     
   }
 }
